2a0b49b7172594ce677f1b3f14429c49e6ee73d2
Chris Mailloux <mailloux.cl@gmail.com>
2016-09-12 10:16:18 -0400

    Configure Basic Properties of Main Window. Change background to black, Change window title, increase size of window (final size will be changed later).

--- Fallout Terminal/Fallout Terminal/Form1.Designer.cs
+++ Fallout Terminal/Fallout Terminal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fallout_Terminal
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(758, 496);
+            this.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "MainWindow";
+            this.Text = "Fallout 4 Terminal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
