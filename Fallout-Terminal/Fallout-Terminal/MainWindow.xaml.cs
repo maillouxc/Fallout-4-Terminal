9526a491c73d8d00a89410e89e2db67e820904f1
Chris Mailloux <mailloux.cl@gmail.com>
2016-09-12 19:52:41 -0400

    Revert "Remove unneeded directories and files from old Windows Forms UI"
    This reverts commit a9cbee9ea482a50b20615bb01fa4b37acfe66edc.

---
+++ Fallout-Terminal/Fallout-Terminal/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fallout_Terminal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void powerButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO Add Logic for Power Button.
+        }
+
+        private void TerminalScreen_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // TODO Determine what logic, if any, needs to go here.
+        }
+    }
+}
