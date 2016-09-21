01977f02fa26ff98028f9d4d3f43d6ae1c0ed5c2
Chris Mailloux <mailloux.cl@gmail.com>
2016-09-21 11:04:57 -0400

    Add algorithm to generate only passwords with certain number of letters in common.

--- Fallout-Terminal/Fallout-Terminal/Source/Logic/PasswordsManager.cs
+++ Fallout-Terminal/Fallout-Terminal/Source/Logic/PasswordsManager.cs
@@ -1,4 +1,6 @@
-﻿namespace Fallout_Terminal.Source.Logic
+﻿using System.Collections.Generic;
+
+namespace Fallout_Terminal.Source.Logic
 {
     /// <summary>
     /// Handles everything to do with how the rest of the game interacts with passwords.
@@ -65,6 +67,61 @@
             }
 
             return numberCorrect;
+        }
+
+        internal List<string> GetPasswordsWithEnoughLettersInCommon(int howManyPasswords, int passwordLength, int howManyInCommon)
+        {
+            bool enoughLettersInCommon = false;
+            List<string> passwords = new List<string>();
+            // Keep generating lists until we get one with enough in common.
+            // This could be O(scary), especially if the RNG is unkind, but we will see.
+            // There is almost certainly a better way to do this, but that is a job for later.
+            while (enoughLettersInCommon == false)
+            {
+                passwords = passwordGenerator.GeneratePotentialPasswords(howManyPasswords, passwordLength);
+                enoughLettersInCommon = EnoughLettersInCommon(passwords, howManyInCommon);
+            }
+            return passwords;
+        }
+
+        private bool EnoughLettersInCommon(List<string> passwords, int howManyLetters)
+        {
+            int lettersInCommon = 0;
+            // HERE BE DRAGONS!!!
+            // For each password in the list of passwords...
+            for(int passwordToCompareTo = 0; passwordToCompareTo < passwords.Count; passwordToCompareTo++)
+            {
+                // We want to compare each character in this password...
+                for(int characterToCompareTo = 0; characterToCompareTo < passwords[0].Length; characterToCompareTo++)
+                {
+                    // Against every other password...
+                    for (int password = passwordToCompareTo; password < passwords.Count; password++)
+                    {
+                        // At the corresponding character.
+                        for (int character = 0; character < passwords[0].Length; character++)
+                        {
+                            // If the password to look at is not the same is the password we are comparing against...
+                            if((password == passwordToCompareTo) == false)
+                            {
+                                // If the characters are equal...
+                                if(passwords[passwordToCompareTo][characterToCompareTo] 
+                                    == passwords[password][character])
+                                {
+                                    lettersInCommon++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if(lettersInCommon >= howManyLetters)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
