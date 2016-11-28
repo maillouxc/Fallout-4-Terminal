145cc3550e4e8075aef3fd9715a694f411a732a1
Chris <mailloux.cl@gmail.com>
2016-11-27 19:10:45 -0500

    Entering incorrect password causes the number of attempts to decrease.

--- Fallout-Terminal/Fallout-Terminal/Model/InputColumn.cs
+++ Fallout-Terminal/Fallout-Terminal/Model/InputColumn.cs
@@ -8,7 +8,7 @@ namespace Fallout_Terminal.Model
 {
     public class InputColumn
     {
-        public string[] Contents { get; set; } 
+        public string[] Contents;
 
         /// <summary>
         /// Creates an InputColumn object.
@@ -16,6 +16,32 @@ namespace Fallout_Terminal.Model
         public InputColumn()
         {
             Contents = new string[TerminalModel.NUMBER_OF_LINES];
+        }
+
+        /// <summary>
+        /// Adds a new line of text to the bottom line of the input column, moving all other lines up 1,
+        /// and erasing the topmost line, if the column is at max height.
+        /// </summary>
+        public void AddLine(string line)
+        {
+            MoveAllLinesUpByOne();
+            Contents[TerminalModel.NUMBER_OF_LINES - 1] = line;
+            // TODO: Notify the program with an event.
+        }
+
+        /// <summary>
+        /// Moves all lines within the input column array up by one, erasing the topmost line
+        /// if the input column is full.
+        /// </summary>
+        public void MoveAllLinesUpByOne()
+        {
+            string[] temp = new string[TerminalModel.NUMBER_OF_LINES];
+            for(int i = 1; i < Contents.Length; i++)
+            {
+                temp[i - 1] = Contents[i];
+                Console.WriteLine(temp[i-1]);
+            }
+            Contents = temp;
         }
     }
 }
